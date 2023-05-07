36215160a48cd3b6362715acc575dbd29da9fd27
Hazem Tarek Elaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2023-05-07 15:32:11 +0300

    feat: preprocess sample images

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from pre_processing import *
+from model_training import *
+from feature_extraction import *
+
+if __name__ == "__main__":
+    pass
+    # test functions here
---
+++ src/pre_processing/pre_processing.py
@@ -0,0 +1,5 @@
+from typing import *
+import numpy as np
+from skimage import io
+import os
+import matplotlib.pyplot as plt
